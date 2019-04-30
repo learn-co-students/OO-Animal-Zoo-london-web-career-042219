cd038cfdfb9abf31beba123bbfb5726e49ff43eb
AmariEllington <amari.l.ellington@gmail.com>
2019-04-30 23:09:04 +0100

    complete

--- lib/Zoo.rb
+++ lib/Zoo.rb
@@ -1,3 +1,42 @@
 class Zoo
 
+
+attr_reader :name, :location
+
+@@all = []
+
+def initialize(name,location)
+    @name = name
+    @location = location
+    @@all << self
+end
+
+def self.all
+    @@all
+end
+
+def current_home
+    Home.all.select { |home| home.zoo == self }
+end
+
+def animal
+    current_home.map { |home| home.animal }
+end
+
+def animal_species  
+    animal.map { |a| a.species }.uniq
+end
+
+def animal_nicknames
+    animal.map { |a| a.nickname }
+end
+
+def animal_species(species)
+    animal.select { |a| a.species.name == species }
+end
+
+def self.find_by_location(location)
+    Home.all.select { |home| home.zoo.location == location }
+end
+
 end
